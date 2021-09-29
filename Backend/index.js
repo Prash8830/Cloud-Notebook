1e8777ebf765794d5a772cba47200afc2d0abfe0
Prashant Rohidas Patil <prashantpatil16012001@gmail.com>
2021-09-29 21:47:00 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,13 @@
 const connecToMongo = require('./db');
-connecToMongo();
 const express = require('express')
+
+connecToMongo();   
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Prash!')
-})
+//available routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes') );
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
