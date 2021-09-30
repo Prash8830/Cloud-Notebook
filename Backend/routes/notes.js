0ef5135fc9e1d17bfbad3febc040b0ad86b28c23
Prashant Rohidas Patil <prashantpatil16012001@gmail.com>
2021-09-30 22:39:52 +0530

    added notes and fetch otes

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,9 +1,46 @@
 const express = require('express');
+const Note = require('../models/Note');
 const router = express.Router();
+var fetchuser = require('../middleware/fetchuser');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res)=>{
-    
-    res.json(["Hi these are my notes"])
-} )
+// Route 1 : Fetch All Notes using = "api/notes/fetchallnotes" using get
+
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id })
+        res.json(notes)
+    }catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error occured");
+    }
+})
+
+
+// Route 2 : add Notes using = "api/notes/addnotes" using post
+
+router.post('/addnotes', fetchuser, [
+    body('title', ' enter a valid title ').isLength({ min: 3 }),
+    body('description', ' enter a valid description ').isLength({ min: 5 }),
+], async (req, res) => {
+
+    try {
+        const { title, description, tag } = req.body;
+
+        //if there are errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        })
+        const saveNote = await note.save()
+        res.json(saveNote)
+    }catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error occured");
+    }
+})
 
 module.exports = router
