bcecd6c173412d070c51c6bce852b014b2a0dc37
Prashant Rohidas Patil <prashantpatil16012001@gmail.com>
2021-10-01 18:15:58 +0530

    Notes Load Succesful

--- Backend/index.js
+++ Backend/index.js
@@ -10,5 +10,5 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes') );
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Cloud Notebook Backend listening at http://localhost:${port}`)
 })
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,10 +1,48 @@
-import NoteContext from "./NoteContext";
+import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props)=>{
-
+    const notesinitial = [
+        {
+          "_id": "6155ee8a671fd11081f25bb9",
+          "user": "6155e3eca8c15f5975edda64",
+          "title": " Updated Trees and Graphs Notes",
+          "description": "Data Structue today Notes",
+          "tag": "DSA",
+          "date": "2021-09-30T17:06:18.929Z",
+          "__v": 0
+        },
+        {
+          "_id": "6155eeef671fd11081f25bbd",
+          "user": "6155e3eca8c15f5975edda64",
+          "title": "Trees and Graphs Notes",
+          "description": "Data Structue Yesterday Notes",
+          "tag": "DSA",
+          "date": "2021-09-30T17:07:59.216Z",
+          "__v": 0
+        },
+        {
+          "_id": "6155eeef671fd11081f25bbf",
+          "user": "6155e3eca8c15f5975edda64",
+          "title": "Trees and Graphs Notes",
+          "description": "Data Structue Yesterday Notes",
+          "tag": "DSA",
+          "date": "2021-09-30T17:07:59.383Z",
+          "__v": 0
+        },
+        {
+          "_id": "6155eeef671fd11081f25bc1",
+          "user": "6155e3eca8c15f5975edda64",
+          "title": "Trees and Graphs Notes",
+          "description": "Data Structue Yesterday Notes",
+          "tag": "DSA",
+          "date": "2021-09-30T17:07:59.610Z",
+          "__v": 0
+        }
+      ]
+      const [notes, setNotes] = useState(notesinitial)
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes,setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
